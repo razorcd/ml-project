0a3f7b6da6ff406182eefefff760dc57d4114b0a
Cristian Dugacicu <razorcd@yahoo.com>
2021-12-12 17:15:03 +0100

    adds build model python script

--- capstone-project/README.md
+++ capstone-project/README.md
@@ -34,7 +34,7 @@ Jupiter notebook has progress comments on each stept.
     - Columns selected for training: 
         - `x = [cellar	baseRent	livingSpace	noRooms	heating	neighbourhoods]`,
         - `y = 'baseRent'` (numerical)
-    - found `MAE = 257.2` and `Model max deviation 50.00: 15.714 percent`
+    - found `MAE = 257.2` and `Model max deviation for 50: 15.714 percent`
 3. Trained a xgboost model:
     - source: [capstoneProject__xgboost.ipynb](capstoneProject__xgboost.ipynb)
     - notice this notebook is linked to the data_analysis notebook
@@ -43,7 +43,69 @@ Jupiter notebook has progress comments on each stept.
     - Columns selected for training: 
         - `x = [newlyConst	balcony	hasKitchen	cellar	baseRent	livingSpace	lift	noRooms	garden	heating	neighbourhoods]`,
         - `y = 'baseRent'` (numerical)
-    - found `MAE = 223.181` and `Model max deviation 50.00: 27.198 percent`
+    - found `MAE = 223.181` and `Model max deviation for 50: 27.198 percent`
+4. Built model_training script using xgboost because it was most acurate.
+    - source: [server/train_model.py](server/train_model.py)
+
+
+
+# Steps to run the application.
+
+## Build final model
+Follow commands:
+
+```bash
+(base) ➜  project1 git:(main) ✗ cd server 
+
+(base) ➜  server git:(main) ✗ pipenv install
+Installing dependencies from Pipfile.lock (0baaa4)...
+  🎃   ▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉ 0/0 — 00:00:00
+
+(base) ➜  server git:(main) ✗ python train_model.py
+Data file loaded. records count: 10406
+doing validation with eta=0.6, max_depth=30
+mae: 232.374
+mae: 236.741
+mae: 229.765
+mae: 252.842
+mae: 243.276
+mae: 230.413
+mae: 243.122
+mae: 230.230
+mae: 233.807
+mae: 232.513
+validation mean mae=236.000, +-7.183
+training the final model. records count= 8324
+test mae=231.67433588150712
+the model is saved to model_xg_0.6_30.bin
+
+(base) ➜  server git:(main) ✗ ls
+model_xg_0.6_30.bin  Pipfile  Pipfile.lock  train_model.py
+
+```
+
+## Start python server using Python
+```bash
+(base) ➜  server git:(main) ✗ python serve.py
+ * Serving Flask app "low_income" (lazy loading)
+ * Environment: production
+   WARNING: This is a development server. Do not use it in a production deployment.
+   Use a production WSGI server instead.
+ * Debug mode: on
+ * Running on http://0.0.0.0:9696/ (Press CTRL+C to quit)
+ * Restarting with inotify reloader
+ * Debugger is active!
+ * Debugger PIN: 381-893-622
+```
+
+### Start python server using Unicorn
+```bash
+(base) ➜  server git:(main) ✗ gunicorn --bind 0.0.0.0:9696 serve:app               
+[2021-10-31 11:25:28 +0100] [28920] [INFO] Starting gunicorn 20.1.0
+[2021-10-31 11:25:28 +0100] [28920] [INFO] Listening at: http://0.0.0.0:9696 (28920)
+[2021-10-31 11:25:28 +0100] [28920] [INFO] Using worker: sync
+[2021-10-31 11:25:28 +0100] [28922] [INFO] Booting worker with pid: 28922
+```
 
 ## TODO ckecklist:
 
---
+++ capstone-project/server/train_model.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pickle
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+
+import xgboost as xgb
+from sklearn.feature_extraction import DictVectorizer
+from sklearn import metrics
+
+
+# parameters
+
+eta = 0.6
+max_depth = 30
+output_file = f'model_xg_{eta}_{max_depth}.bin'
+
+
+# data preparation
+
+df = pd.read_csv('../immo_data_final.csv')
+print("Data file loaded. records count:", len(df))
+# columns = [
+#     'newlyConst', 
+#     'balcony', 
+#     'hasKitchen', 
+#     'cellar', 
+#     'baseRent',
+#     'livingSpace', 
+#     'lift', 
+#     'noRooms', 
+#     'garden', 
+#     'heating',
+#     'neighbourhoods'
+# ]
+# df.columns = columns
+
+# column types
+categorical_columns = [
+    'neighbourhoods', # 'Spandau' 'Weißensee' 'Mitte' 'Kreuzberg' 'Tiergarten' 'Köpenick' Marzahn' 'Hohenschönhausen_Hohenschönhausen' 'Hellersdorf' 'Berg_Prenzlauer_Berg' 'Buchholz_Pankow' 'Charlottenburg' 'Tempelhof' 'Neukölln' 'Wilmersdorf' 'Wedding' 'Pankow' 'Friedrichshain' 'Reinickendorf' 'Treptow' 'Schöneberg' 'Lichtenberg' 'Steglitz' 'Zehlendorf' 'Hohenschönhausen'
+    'heating', # low, normal, high
+]
+numerical_columns = [
+    'newlyConst', 
+    'balcony', 
+    'hasKitchen', 
+    'cellar', 
+    'baseRent',
+    'livingSpace', 
+    'lift', 
+    'noRooms', 
+    'garden', 
+]
+
+y_column = 'baseRent'
+
+#Split data
+from sklearn.model_selection import train_test_split
+def split_data(data, split1, split2):
+    df_full_train, df_test = train_test_split(data, test_size=0.2)
+    df_train,  df_val = train_test_split(df_full_train, test_size=0.25)
+
+    df_full_train = df_full_train.reset_index(drop=True)
+    df_train = df_train.reset_index(drop=True)
+    df_val = df_val.reset_index(drop=True)
+    df_test = df_test.reset_index(drop=True)
+
+    y_full_train = df_full_train.baseRent
+    y_train = df_train.baseRent
+    y_val = df_val.baseRent
+    y_test = df_test.baseRent
+
+    del df_full_train["baseRent"]
+    del df_train["baseRent"]
+    del df_val["baseRent"]
+    del df_test["baseRent"]
+
+    return df_full_train, df_train, df_val, df_test, y_full_train, y_train, y_val, y_test
+
+#Training 
+
+def train(dataFrame, y, max_depth, eta):
+    # Hot Encoding
+    dicts = dataFrame.to_dict(orient="records")
+    dv = DictVectorizer(sparse=False)
+    X = dv.fit_transform(dicts)
+    features = dv.get_feature_names()
+    dtrain = xgb.DMatrix(X, label=y, feature_names=features)
+
+    # train
+    xgb_params = {
+        'eta': eta,
+        'max_depth': max_depth,
+        'min_child_weight': 1,
+        'objective': 'reg:squarederror',
+        'nthread': 8,
+        'seed':1,
+        'verbosity':0
+    }
+    model = xgb.train(xgb_params, dtrain, num_boost_round=10)
+    return dv, model
+
+def predict(dataFrame, dv, model):
+    dicts = dataFrame.to_dict(orient="records")
+    X = dv.transform(dicts)
+    features = dv.get_feature_names()
+    dval = xgb.DMatrix(X, feature_names=features)
+    y_pred = model.predict(dval)
+    return y_pred, X 
+
+
+# validation
+
+print(f'doing validation with eta={eta}, max_depth={max_depth}')
+
+scores = []
+for i in range(0, 10):
+    df_full_train, df_train, df_val, df_test, y_full_train, y_train, y_val, y_test = split_data(df, split1=0.2, split2=0.25)
+    dv, model = train(df_train, y_train, max_depth=max_depth, eta=eta)
+    y_pred_val, X_val = predict(df_val, dv, model)
+
+    mae = metrics.mean_absolute_error(y_val, y_pred_val)
+    print("mae: %.3f" % (mae))
+    scores.append(int(mae))
+    
+
+print('validation mean mae=%.3f, +-%.3f' % (np.mean(scores), np.std(scores)))
+
+
+# training the final model
+
+print('training the final model. records count=', len(df_full_train))
+
+df_full_train, df_train, df_val, df_test, y_full_train, y_train, y_val, y_test = split_data(df, split1=0.2, split2=0.25)
+
+final_dv, final_model = train(df_full_train, y_full_train, max_depth=max_depth, eta=eta)
+y_pred_test, X_test = predict(df_test, final_dv, final_model)
+
+mae_test = metrics.mean_absolute_error(y_test, y_pred_test)
+print(f'test mae={mae_test}')
+
+
+# # Save the model
+with open(output_file, 'wb') as f_out:
+    pickle.dump((dv, model), f_out)
+
+print(f'the model is saved to {output_file}')
